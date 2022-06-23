d56c9042349ba3b53f0ad59b0098a52e801eae78
TheH2SO4 <theh2so4@protonmail.com>
2022-06-23 14:04:52 +0100

    Update

--- hybrid/nodejs/entrypoint.sh
+++ hybrid/nodejs/entrypoint.sh
@@ -26,28 +26,6 @@ cd /home/container # || Changing container's working directory.
 
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'` # || Set a local variable that makes the private Docker IP address available for the processes.
 
-toilet --filter border:gay AcidicNodes
-echo
-
-sleep 0.2
-
-printf "\033[1m\033[33mcontainer@acidicnodes~ \033[0mShowing NodeJs information...\n"
-echo
-
-sleep 0.1
-
-printf "\033[1m\033[33mcontainer@acidicnodes~ \033[0mNodeJs version: $(node -v)\n" # || Show the actual NodeJs version.
-
-printf "\033[1m\033[33mcontainer@acidicnodes~ \033[0mNPM version: $(npm -v)\n" # || Show the actual NPM version.
-
-# printf "\033[1m\033[33mcontainer@acidicnodes~ \033[0mYarn version: $(yarn version check)\n" # || Show the actual Yarn version. 
-
-# printf "\033[1m\033[33mcontainer@acidicnodes~ \033[0mPNPM version: $(pnpm version)\n" # || Show the actual PNPM version. 
-
-printf "\033[1m\033[33mcontainer@acidicnodes~ \033[0mPython version: $(python3 --version)\n" # || Show the actual Python version.
-
-sleep 0.2
-
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g') # || Replace the Startup command.
 
 eval ${MODIFIED_STARTUP} &> /dev/null # || Start the server.
